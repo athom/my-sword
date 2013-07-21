92abda6b9d199afc8fcb8475b8da40e556cdf40b
yeer <yeerkunth+github@gmail.com>
2013-07-21 23:36:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,34 @@
 #! /bin/sh
 
-## pull
-#if [ -d ~/.vim ]
-#then
-#  mv ~/.vim ~/.vim-bk
-#fi
-#git clone http://github.com/athom/my-sword.git ~/.vim
-#
-## install vundle
-#if [ ! -d ~/.vim/bundle/vundle ]
-#then
-#  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-#fi
-#
-#echo "link configurations"
-## rename .vimrc
-#if [ -f ~/.vimrc ]
-#then
-#  mv ~/.vimrc ~/.vimrc-bk
-#fi
-#
-## rename .gvimrc
-#if [ -f ~/.gvimrc ]
-#then
-#  mv ~/.gvimrc ~/.gvimrc-bk
-#fi
-#
-## link configurations
-#ln -s ~/.vim/_vimrc ~/.vimrc
-#ln -s ~/.vim/_gvimrc ~/.gvimrc
+# pull
+if [ -d ~/.vim ]
+then
+  mv ~/.vim ~/.vim-bk
+fi
+git clone http://github.com/athom/my-sword.git ~/.vim
+
+# install vundle
+if [ ! -d ~/.vim/bundle/vundle ]
+then
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+echo "link configurations"
+# rename .vimrc
+if [ -f ~/.vimrc ]
+then
+  mv ~/.vimrc ~/.vimrc-bk
+fi
+
+# rename .gvimrc
+if [ -f ~/.gvimrc ]
+then
+  mv ~/.gvimrc ~/.gvimrc-bk
+fi
+
+# link configurations
+ln -s ~/.vim/_vimrc ~/.vimrc
+ln -s ~/.vim/_gvimrc ~/.gvimrc
 
 # install plugins
 echo "install plugins"
