a5c2b1d1f76f77ff6bb6e6c8e4fcd072a26e2bfc
athom <athom@126.com>
2013-07-21 22:49:43 +0800

    improve install script

--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
 #! /bin/sh
 
+
+# install vundle
+if [ ! -d ~/.vim/bundle/vundle ]
+then
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+# rename .vimrc
 if [ -f ~/.vimrc ]
 then
   mv ~/.vimrc ~/.vimrc-bk
 fi
 
+# rename .gvimrc
 if [ -f ~/.gvimrc ]
 then
   mv ~/.gvimrc ~/.gvimrc-bk
 fi
 
+# link configurations
 path=`pwd`
 ln -s $path/_vimrc ~/.vimrc
 ln -s $path/_gvimrc ~/.gvimrc
+
+# install plugins
+vim +BundleInstall +qa
